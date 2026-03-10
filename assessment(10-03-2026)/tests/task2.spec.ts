05dc4d109d321f5f5f29678aa37767f54f352636
Nimish57 <nimishkhandelwal284@gmail.com>
2026-03-10 21:21:09 +0530

    task uploaded

---
+++ assessment(10-03-2026)/tests/task2.spec.ts
@@ -0,0 +1,7 @@
+import {test} from "@playwright/test"
+test("lenskart",async({page})=>{ 
+    await page.goto('https://www.lenskart.com/');
+    await page.locator('//a[@id="lrd9"]').hover();
+    await page.locator('//a[@href="https://www.lenskart.com/stores/location/karnataka/bengaluru"]').click();
+    await page.screenshot({path:"screenshot/task2.png"});
+})
