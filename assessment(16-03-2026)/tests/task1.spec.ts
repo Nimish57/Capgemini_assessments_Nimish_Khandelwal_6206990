1a423b9b6fa4e9189ee1c81e1dfd229e152f9ccd
Nimish57 <nimishkhandelwal284@gmail.com>
2026-03-16 19:41:45 +0530

    task uploaded

---
+++ assessment(16-03-2026)/tests/task1.spec.ts
@@ -0,0 +1,28 @@
+import {test} from "@playwright/test"
+import flipkart from "../pom/pom.spec"
+import path from "path";
+import fs from "fs"
+let fkart=JSON.parse(fs.readFileSync(path.join(__dirname,"../testdata/data.json")));
+test("flipkart",async({page})=>{
+    let flipkartsite=new flipkart();
+    await flipkartsite.load_flipkart(page,fkart.url);
+    await page.locator('//span[.="✕"]').click();
+    await flipkartsite.gudipadwa(page);
+    await flipkartsite.gudicloth(page);
+    let [page2] = await Promise.all([page.waitForEvent('popup'), flipkartsite.clickproduct(page,5)]);
+    await page2.waitForTimeout(2000);
+    await flipkartsite.add_cart(page2);
+    page.bringToFront();
+    let [page3] = await Promise.all([page.waitForEvent('popup'), flipkartsite.clickproduct(page,3)]);
+    await page3.waitForTimeout(2000);
+    await flipkartsite.add_cart(page3);
+    page.bringToFront();
+    await flipkartsite.to_cart(page);
+    await page.waitForTimeout(2000);
+    await flipkartsite.quantity(page,1,2);
+    await page.screenshot({path:"screenshot/task1-1.png"})
+    await flipkartsite.quantity(page,2,3);
+    await page.screenshot({path:"screenshot/task1-2.png"})
+    await flipkartsite.place_order(page);
+    await page.screenshot({path:"screenshot/task1-3.png"})
+})
